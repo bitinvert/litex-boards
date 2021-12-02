c8bcaf9f4b34142e64e827497c1d912fabfb539e
Brendan Christy <brendan@pop-os.localdomain>
2021-12-02 09:47:20 +0100

    Adding support for the Alinx AX7101

---
+++ litex_boards/platforms/alinx_ax7101.py
@@ -0,0 +1,153 @@
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2021 Brendan Christy <brendan.christy@hs-rm.de>
+# SPDX-License-Identifier: BSD-2-Clause
+
+from litex.build.generic_platform import *
+from litex.build.xilinx import XilinxPlatform, VivadoProgrammer
+from litex.build.openocd import OpenOCD
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+    # Clk / Rst
+    ("clk200_p", 0, Pins("R4"), IOStandard("DIFF_SSTL15")),
+    ("clk200_n", 0, Pins("T4"), IOStandard("DIFF_SSTL15")),
+    ("clk125_p", 0, Pins("F6"), IOStandard("DIFF_SSTL15")),
+    ("clk125_n", 0, Pins("E6"), IOStandard("DIFF_SSTL15")),
+    ("cpu_reset", 0, Pins("T6"), IOStandard("DIFF_SSTL15")),
+
+    # DDR3 SDRAM
+    ("ddram", 0,
+        Subsignal("a", Pins("AA4 AB2 AA5 AB5 AB1 U3 W1 T1 V2 U2 Y1 W2 Y2 U1 V3"), IOStandard("SSTL15")),
+        Subsignal("ba", Pins("AA3 Y3 Y4"), IOStandard("SSTL15")),
+        Subsignal("ras_n", Pins("V4"), IOStandard("SSTL15")),
+        Subsignal("cas_n", Pins("W4"), IOStandard("SSTL15")),
+        Subsignal("we_n", Pins("AA1"), IOStandard("SSTL15")),
+        Subsignal("dm", Pins("D2 G2 M2 M5"), IOStandard("SSTL15")),
+        Subsignal("dq", Pins("C2 G1 A1 F3 B2 F1 B1 E2 H3 G3 H2 H5 J1 J5 K1 H4 L4 M3 L3 J6 K3 K6 J4 L5 P1 N4 R1 N2 M6 N5 P6 P2"), IOStandard("SSTL15"), Misc("IN_TERM=UNTUNED_SPLIT_50")),
+        Subsignal("dqs_p", Pins("E1 K2 M1 P5"), IOStandard("DIFF_SSTL15")),
+        Subsignal("dqs_n", Pins("D1 J2 L1 P4"), IOStandard("DIFF_SSTL15")),
+        Subsignal("clk_p", Pins("R3"), IOStandard("DIFF_SSTL15")),
+        Subsignal("clk_n", Pins("R2"), IOStandard("DIFF_SSTL15")),
+        Subsignal("cke", Pins("T5"), IOStandard("SSTL15")),
+        Subsignal("odt", Pins("U5"), IOStandard("SSTL15")),
+        Subsignal("cs_n", Pins("AB3"), IOStandard("SSTL15")),
+        Subsignal("rst_n", Pins("W6"), IOStandard("LVCMOS15")),
+        Misc("SLEW=FAST"),
+    ),
+
+    # UART
+    ("uart", 0,
+        Subsignal("tx", Pins("AB15")),
+        Subsignal("rx", Pins("AA15")),
+        IOStandard("LVCMOS33"),
+    ),
+
+    # GMII Ethernet
+    ("eth_clocks", 0,
+        Subsignal("gtx", Pins("G21")),
+        Subsignal("tx", Pins("K21")),
+        Subsignal("rx", Pins("K18")),
+        IOStandard("LVCMOS33")
+    ),
+    ("eth_clocks", 1,
+        Subsignal("gtx", Pins("M16")),
+        Subsignal("tx", Pins("T14")),
+        Subsignal("rx", Pins("J20")),
+        IOStandard("LVCMOS33")
+    ),
+    ("eth_clocks", 2,
+        Subsignal("gtx", Pins("AA21")),
+        Subsignal("tx", Pins("V10")),
+        Subsignal("rx", Pins("V13")),
+        IOStandard("LVCMOS33")
+    ),
+    ("eth_clocks", 3,
+        Subsignal("gtx", Pins("P20")),
+        Subsignal("tx", Pins("U16")),
+        Subsignal("rx", Pins("Y18")),
+        IOStandard("LVCMOS33")
+    ),
+    ("eth", 0,
+        Subsignal("tx_data", Pins("D22 H20 H22 J22 K22 L19 K19 L20")),
+        Subsignal("tx_en",   Pins("G22")),
+        Subsignal("tx_er",   Pins("K17")),
+        Subsignal("rx_dv",   Pins("M22")),
+        Subsignal("rx_er",   Pins("N18")),
+        Subsignal("rx_data", Pins("N22 H18 H17 M21 L21 N20 M20 L19")),
+        Subsignal("col_dv",  Pins("M18")),
+        Subsignal("crs_dv",  Pins("L18")),
+        Subsignal("rst_n",   Pins("G20")),
+        Subsignal("mdc",     Pins("J17")),
+        Subsignal("mdio",    Pins("L16")),
+        IOStandard("LVCMOS33")
+    ),
+    ("eth", 1,
+        Subsignal("tx_data", Pins("L15 K16 W15 W16 V17 W17 U15 V15")),
+        Subsignal("tx_en",   Pins("M15")),
+        Subsignal("tx_er",   Pins("T15")),
+        Subsignal("rx_dv",   Pins("L13")),
+        Subsignal("rx_er",   Pins("G13")),
+        Subsignal("rx_data", Pins("M13 K14 K13 J14 H14 H15 J15 H13")),
+        Subsignal("col_dv",  Pins("J11")),
+        Subsignal("crs_dv",  Pins("E22")),
+        Subsignal("rst_n",   Pins("L14")),
+        Subsignal("mdc",     Pins("AB21")),
+        Subsignal("mdio",    Pins("AB22")),
+        IOStandard("LVCMOS33")
+    ),
+    ("eth", 2,
+        Subsignal("tx_data", Pins("W11 W12 Y11 Y12 W10 AA11 AA10 AB10")),
+        Subsignal("tx_en",   Pins("V14")),
+        Subsignal("tx_er",   Pins("AA9")),
+        Subsignal("rx_dv",   Pins("AA20")),
+        Subsignal("rx_er",   Pins("U21")),
+        Subsignal("rx_data", Pins("AB20 AA19 AA18 AB18 Y17 W22 W21 T21")),
+        Subsignal("col_dv",  Pins("Y21")),
+        Subsignal("crs_dv",  Pins("Y22")),
+        Subsignal("rst_n",   Pins("T20")),
+        Subsignal("mdc",     Pins("V20")),
+        Subsignal("mdio",    Pins("V19")),
+        IOStandard("LVCMOS33")
+    ),
+    ("eth", 3,
+        Subsignal("tx_data", Pins("R17 P15 N17 P17 T16 U17 U18 P19")),
+        Subsignal("tx_en",   Pins("P16")),
+        Subsignal("tx_er",   Pins("R19")),
+        Subsignal("rx_dv",   Pins("W20")),
+        Subsignal("rx_er",   Pins("N13")),
+        Subsignal("rx_data", Pins("W19 Y10 V22 U22 T18 R18 R14 P14")),
+        Subsignal("col_dv",  Pins("N14")),
+        Subsignal("crs_dv",  Pins("N15")),
+        Subsignal("rst_n",   Pins("R16")),
+        Subsignal("mdc",     Pins("V18")),
+        Subsignal("mdio",    Pins("U20")),
+        IOStandard("LVCMOS33")
+    ),
+]
+
+# TODO add SCCard support
+
+_connectors = []
+
+class Platform(XilinxPlatform):
+    default_clk_name   = "clk200_p"
+    default_clk_period = 1e9/200e6
+
+    def __init__(self) -> None:
+        XilinxPlatform.__init__(self, "xc7a100t-fgg484-2", _io, _connectors, toolchain="vivado")
+        self.add_platform_command("set_property INTERNAL_VREF 0.750 [get_iobanks 34]")
+
+
+    def create_programmer(self):
+        return OpenOCD("openocd_ax7101.cfg", "bscan_spi_xc7a100t.bit")
+
+    def do_finalize(self, fragment):
+        XilinxPlatform.do_finalize(self, fragment)
+        self.add_period_constraint(self.lookup_request("clk200_p",        loose=True), 1e9/200e6)
+        
+        self.add_period_constraint(self.lookup_request("eth_clocks:gtx", loose=True), 1e9/125e6)
+        self.add_period_constraint(self.lookup_request("eth_clocks:tx", loose=True), 1e9/125e6)
+        self.add_period_constraint(self.lookup_request("eth_clocks:rx", loose=True), 1e9/125e6)
---
+++ litex_boards/targets/alinx_ax7101.py
@@ -0,0 +1,107 @@
+import os
+import argparse
+from migen import *
+
+from litex_boards.platforms import alinx_ax7101
+
+from litex.soc.cores.clock import *
+from litex.soc.integration.soc import SoCRegion
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+
+from litedram.modules import MT41J256M16
+from litedram.phy import s7ddrphy
+
+from liteeth.phy.gmii import *
+
+class _CRG(Module):
+    def __init__(self, platform, sys_clk_freq):
+        self.rst = Signal()
+        self.clock_domains.cd_sys       = ClockDomain()
+        self.clock_domains.cd_sys2x     = ClockDomain(reset_less=True)
+        self.clock_domains.cd_idelay    = ClockDomain()
+
+        self.submodules.pll = pll = S7MMC(speedgrade=-2)
+        self.comb += pll.reset.eq(~platform.request("cpu_reset") | self.rst)
+        #TODO fill the function
+        pll.register_clkin(platform.request("clk200_p"), 200e6)
+        pll.create_clkout(self.cd_sys,    sys_clk_freq)
+        pll.create_clkout(self.cd_sys2x,  2*sys_clk_freq)
+        pll.create_clkout(self.cd_idelay, 200e6)
+        platform.add_false_path_constraints(self.cd_sys.clk, pll.clkin)
+
+        self.submodules.idelayctrl = S7DELAYCTRL(self.cd_idelay)
+
+
+class BaseSoC(SoCCore):
+    def __init__(self, sys_clk_freq, **kwargs):
+        platform = alinx_ax7101.Platform()
+
+        # SoCCore ---------------------------------------------------------
+        SoCCore.__init__(self, platform, sys_clk_freq, 
+                         ident = "LiteX SoC on Alinx AX7101",
+                         ident_version = True,
+                         **kwargs)
+
+        # CRG -------------------------------------------------------------
+        self.submodules.crg = _CRG(platform, sys_clk_freq) 
+
+        # DDR3 SDRAM ------------------------------------------------------
+        if not self.integrated_main_ram_size:
+            self.submodules.ddrphy = s7ddrphy.A7DDRPHY(platform.request("ddram"),
+                memtype = "DDR3",
+                nphase  = 4,
+                sys_clk_freq = sys_clk_freq)
+            self.add_sdram("sdram",
+                phy           = self.ddrphy,
+                module        = MT41J256M16(sys_clk_freq, "1:4"),
+                l2_cache_size = kwargs.get("l2_size", 8192)
+            )
+        
+        # Ethernet -------------------------------------------------------
+        self.submodules.ethphy = LiteEthPHYGMII(
+            clock_pads = self.plaform.request("eth_clocks"),
+            pads       = self.platform.request("eth")
+        )
+        self.add_ethernet(phy=self.ethphy)
+            
+
+def main():
+    parser = argparse.ArgumentParser(description="LiteX SoC on Alinx AX7101")
+    parser.add_argument("--build", action="store_true", help="Build Bitstream")
+    parser.add_argument("--load", action="store_true", help="Load Bitstream")
+    parser.add_argument("--sys-clk-freq", default=200e6, help="System clock frequency (default: 200MHz)")
+    
+    #TODO Add SDCard support
+    """
+    sdopts = parser.add_mutually_exclusive_group()
+    sdopts.add_argument("--with-spi-sdcard", action="store_true", help="Enable SPI-mode SDCard support")
+    sdopts.add_argument("--with-sdcard", action="store_true", help="Enable SDCard support")
+    """
+    
+    builder_args(parser)
+    soc_core_args(parser)
+    args = parser.parse_args()
+
+    soc = BaseSoC(
+        sys_clk_freq=int(float(args.sys_clk_freq)),
+        **soc_core_argdict(args)
+    )
+
+    #TODO SDCard support
+    """
+    if args.with_spi_sdcard:
+        soc.add_spi_sdcard()
+    if args.with_sdcard:
+        soc.add_sdcard()
+    """
+
+    builder = Builder(soc, **builder_argdict(args))
+    builder.build(run=args.build)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".bit"))
+
+if __name__ == "__main__":
+    main()
